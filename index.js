4fdc98890b73e4a3d68f61693624ccc554260a12
faridho <faridho45@gmail.com>
2017-11-07 12:45:56 +0700

    Get Token Solved

--- index.js
+++ index.js
@@ -16,14 +16,18 @@ app.get('/', function(req, res){
 
 app.post('/auth', function(req, res){
     var sql      = 'SELECT * FROM user WHERE username = "' + req.body.username + '" AND password = "' + req.body.password + '"';
-    var token    = jwt.sign(app.get('superSecret'), {
-        expiresInMinutes: 1440
-    })
     con.query(sql, function(err, result){
+        const payload = {
+            user : result.user_id
+        }
+        var token    = jwt.sign(payload, app.get('superSecret'), {
+            expiresIn: 1440 //24 hours
+        })
+    
         if(result.length > 0){
-            res.json({"Error": false, "Message": "Yeayyy"});
+            res.json({"Error": false, "Message": "Yeayyy", "Token": token});
         }else{
-            res.json({"Error": true, "Message": "Username & Password dont match", "Token": token});
+            res.json({"Error": true, "Message": "Username & Password dont match"});
         }
     });
     
